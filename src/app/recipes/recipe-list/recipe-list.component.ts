dae813867472e618d4bd4e6fd4d67095507fc3a4
Aneudy Guzman <aneudy.guzman@bookedin.net>
2024-03-21 17:01:01 -0400

    Display selected recipe details from the recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,12 +11,18 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Rest Recipe', 'This is a simple test', 
     'https://www.foodandwine.com/thmb/YlgBj_G9a_psYSzA3gfU6gx9A3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bucatini-with-mushroom-ragu-dandelion-greens-and-tarragon-FT-RECIPE0421-3a5f0d29f7264f5e9952d4a3a51f5f58.jpg'),
     
-    new Recipe('A Rest Recipe', 'This is a simple test', 
+    new Recipe('Another Rest Recipe', 'This is a simple test', 
     'https://www.foodandwine.com/thmb/YlgBj_G9a_psYSzA3gfU6gx9A3w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bucatini-with-mushroom-ragu-dandelion-greens-and-tarragon-FT-RECIPE0421-3a5f0d29f7264f5e9952d4a3a51f5f58.jpg')
   
   ];
+
+  @Output() childItemClicked: EventEmitter<Recipe> = new EventEmitter<Recipe>();
   
   ngOnInit(): void {
+  }
+
+  itemClickedHandler(selectedRecipe: Recipe) {
+    this.childItemClicked.emit(selectedRecipe);
   }
 
 }
